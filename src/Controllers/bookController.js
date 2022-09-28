cec44af8e051bc077efa0503dc63288aea875833
anirudh sagar <you@example.com>
2022-09-28 19:32:50 +0530

    update

--- src/Controllers/bookController.js
+++ src/Controllers/bookController.js
@@ -9,7 +9,7 @@ const aws = require('../AWS/aws.js')
 const createBook = async (req, res) => {
     try {
         const data = req.body
-        let { title, excerpt, userId, ISBN, category, subcategory, releasedAt, bookCover, isDeleted } = data
+        let { title, excerpt, userId, ISBN, category, subcategory, releasedAt,  isDeleted } = data
         // If empty request body
         if (!validation.isValidRequest(data)) return res.status(400).send({ status: false, message: "Please enter User data" });
 
@@ -52,7 +52,7 @@ const createBook = async (req, res) => {
         let files = req.files
         if (files && files.length > 0) {
 
-            let uploadedFileURL = await uploadFile(files[0])
+            let uploadedFileURL = await aws.uploadFile(files[0])
             const uniqueCover = await bookModel.findOne({ bookCover: uploadedFileURL });
             if (uniqueCover) { return res.status(400).send({ status: false, message: "book cover already exists" }) }
 
@@ -63,20 +63,6 @@ const createBook = async (req, res) => {
         }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         // releasedAt
         if (!validation.isValid(releasedAt)) return res.status(400).send({ status: false, message: "ReleasedAt is required." });
         if (!validation.isValidDate(releasedAt)) return res.status(400).send({ status: false, message: "please provide releasedAt in correct format" })
@@ -84,7 +70,7 @@ const createBook = async (req, res) => {
         // Can't Set deleted true at creation time
         if (isDeleted == true) return res.status(400).send({ status: false, message: "You can't add this key at book creation time." })
 
-        let savedData = await bookModel.create(req.body)
+        let savedData = await bookModel.create(data)
         return res.status(201).send({ status: true, data: savedData })
     }
     catch (error) {
